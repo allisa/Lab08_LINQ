33315a8aace25f593686e9b3142f90e59303674e
Allisa LeBeuf <al5906@gmail.com>
2018-10-19 11:11:17 -0700

    all queries working

--- Lab08_LINQ/Program.cs
+++ Lab08_LINQ/Program.cs
@@ -27,30 +27,59 @@ namespace Lab08_LINQ
                 text = sr.ReadToEnd();
             }
 
-
             var manhattan = JsonConvert.DeserializeObject<FeatureCollections>(text);
+
+            //this didn't work which is confusing b/c it was from demo code
             //IEnumerable<FeatureCollections> manhattan = JsonConvert.DeserializeObject<IEnumerable<FeatureCollections>>(text);
 
-
+            Console.WriteLine("*********1 - All Neighborhhods*********");
+            //Pulling all neighborhoods from feature collection
             var properties = manhattan.Features.Select(x => x).Select(x => x.Properties).Select(x => x.Neighborhood);
 
             foreach (var item in properties)
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine("*********2 - Filter Out Neighborhoods Without Names*********");
+
+            //Filtering out neighborhoods without names
+            var filterNeighborhoodsWONames = (from result in properties
+                                              where result.Length > 0
+                                              select result);
+
+            foreach (var item in filterNeighborhoodsWONames)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("*********3 - Show Distinc Neighborhoods Names*********");
+
+            //Filtering further to remove duplicate neighborhood names
+            var distinctNeighborhoodNames = filterNeighborhoodsWONames.Distinct();
+
+            foreach (var item in distinctNeighborhoodNames)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("*********4 - Done In One Expression*********");
+
+            var filterAllPrevSolutionIntoOne = manhattan.Features.Select(x => x).Select(x => x.Properties).Select(x => x.Neighborhood).Where(x => x != "").Distinct();
+
+            foreach (var item in filterAllPrevSolutionIntoOne)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("*********5 - Rewrite in opposing method*********");
+            var filterNoNamesLambda = properties.Where(x => x.Length > 0);
+
+            foreach(var item in filterNoNamesLambda)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
-
-
-
-//Read in the file and answer the questions below
-//Use LINQ queries and Lambda statements(when appropriate) to find the answers.
-//Use a combination of both to answer the questions.
-//Each question and answer should be outputted to the console.
-
-//Output all of the neighborhoods in this data list
-//Filter out all the neighborhoods that do not have any names
-//Remove the Duplicates
-//Rewrite the queries from above, and consolidate all into one single query.
-//Rewrite at least one of these questions only using the opposing method(example: Use LINQ instead of a Lambda and vice versa.)
